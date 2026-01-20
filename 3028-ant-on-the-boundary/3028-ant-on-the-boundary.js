6c942af51593db8299a8363af20bd9cd5e95a779
Rahul Radhakrishnan <rahulkrishna1595@gmail.com>
2026-01-20 11:58:25 +0530

    Time: 52 ms (41.89%), Space: 55.4 MB (61.49%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var returnToBoundaryCount = function(nums) {
+    let pos = 0, count =0
+    for(let i=0;i<nums.length;i++) {
+        pos += nums[i]
+        if(pos === 0) {
+            count ++ 
+        }
+    }
+    return count
+};
