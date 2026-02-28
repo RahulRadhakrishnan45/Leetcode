780c8a3b8ba8b4a4638d19f67d57256b5646a419
Rahul Radhakrishnan <rahulkrishna1595@gmail.com>
2026-03-01 01:49:04 +0530

    Time: 5 ms (35.57%), Space: 55.9 MB (53.06%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+   let map = {')':'(' ,']':'[' ,'}':'{'}
+   let stack = []
+   for(let char of s) {
+    if(!map[char]) {
+        stack.push(char)
+    }else if(stack.pop() !== map[char]) {
+        return false
+    }
+   }
+   return stack.length === 0
+};
