d53b9d58a2af51930fd07a5f9c295124d0835536
Rahul Radhakrishnan <rahulkrishna1595@gmail.com>
2026-03-30 15:02:52 +0530

    Time: 6 ms (92.96%), Space: 57.3 MB (54.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,6 +4,14 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length !== t.length) return false
-    return s.split('').sort().join('') === t.split('').sort().join('')
+    if(s.length != t.length)  return false
+    let fre = {}
+    for(let char of s) {
+        fre[char]=(fre[char]||0)+1
+    }
+    for(let char of t) {
+        if(!fre[char]) return false
+        fre[char]--
+    }
+    return true
 };
