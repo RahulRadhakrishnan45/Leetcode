cbc00380a1da0935a7ecbf7d3e0caa059d05d5c5
Rahul Radhakrishnan <rahulkrishna1595@gmail.com>
2026-08-16 16:13:37 +0530

    Time: 2 ms (37.05%), Space: 55.7 MB (67.73%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let c=0
+    for(i=0;i<nums.length-1;i++){
+        for(j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]<target){
+                c++
+            }
+        }
+    }
+    return c
+};
