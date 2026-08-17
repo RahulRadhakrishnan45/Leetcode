4c3bc6b7d6b98248a38320d4655fea21fc45e365
Rahul Radhakrishnan <rahulkrishna1595@gmail.com>
2026-08-17 11:25:44 +0530

    Time: 0 ms (100%), Space: 55.7 MB (34.81%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let a =0
+    let b =0
+    for(i=1;i<=n;i++){
+        if(i%m!=0){
+            a+=i
+        }else{
+            b+=i
+        }
+    }
+    let r = a-b
+    return r
+};
