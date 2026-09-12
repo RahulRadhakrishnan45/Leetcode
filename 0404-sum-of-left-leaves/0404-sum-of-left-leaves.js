4f72ddbe9a835576cd284361eded7be999866293
Rahul Radhakrishnan <rahulkrishna1595@gmail.com>
2026-09-13 01:03:43 +0530

    Time: 0 ms (100%), Space: 56.7 MB (47.39%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    if(!root) return 0
+    let ans =0
+    if(root.left !== null){
+        if(root.left.left === null && root.left.right === null) {
+            ans += root.left.val
+        }else{
+            ans += sumOfLeftLeaves(root.left)
+        }
+    }
+    ans += sumOfLeftLeaves(root.right)
+    return ans
+};
